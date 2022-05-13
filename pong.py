4f32b40f06402e5f8a00b55f815f4095587ede97
Santiago Quihui <santiagoquihui@hotmail.com>
2022-05-13 11:02:58 -0500

    add title to window

--- pong.py
+++ pong.py
@@ -22,6 +22,7 @@ score.write("Left Player :{} Right Player: {}".format( left_paddle_score, right_
 
 # Set up the screen
 setup(width=800, height=600)
+title("Pong")
 
 # Set up the left paddle
 left_paddle.shape("square")
